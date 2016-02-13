6b6ead0db72c3295a0989fbd16da5fc9b3da07c4
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-13 18:37:20 -0500

    Wip changes

--- tttscripts.js
+++ tttscripts.js
@@ -3,43 +3,59 @@ $(document).on('ready', function(){
   var turn = 0;
   var winningCombos = [[]];
 
+  var containerX = $('<div id="containerX"></div>').hide();
+  var containerO = $('<div id="containerO"></div>').hide();
 
+  $('body').append(containerX);
+  $('body').append(containerO);
 
   // Click to create X or O
   $('td').on('click', function(){
-// setting a variable. we want to do the work and do it once
+  // setting a variable. we want to do the work and do it once
     var self = $(this);
-// determin the turn - even turns the click returns an x and on odd turn the click returns an o
+    // determin the turn - even turns the click returns an x and on odd turn the click returns an o
     if ( turn % 2 ){
       self.replaceWith('<td> O </td>');
-        $('#containerY').append(self).hide();
+        $('#containerO').append(self);
+        checkForWinner(document.getElementById('containerO').children);
     } else {
       self.replaceWith('<td> X </td>');
-      $('#containerX').append(self).hide();
+        $('#containerX').append(self);
+        checkForWinner(document.getElementById('containerX').children);
     }
-// this removes the event listener so we can only click once.
+    // this removes the event listener so we can only click once.
     self.off('click');
 
-    checkForWinner();
-// incrementing the turn
     turn++;
 
-  });
+    function checkForWinner(array){
 
+        for (i = 0; i < 9; i++){
+          var square = array[i];
+          var currentScore = parseInt(square);
+          // var currentScore = parseInt(score);
+          checkScore(currentScore);
+        }
+
+        function checkScore(someScore){
+
+
+        if (someScore === 5){
+          alert("you won!");
+        }
+        }
+    }
+});
 
 // specifiy order of turns
 
 // create a loop to allow the users to keep playing untill there is a winner
 
 //evaluate to see if there is a winner
-function checkForWinner(){
-  // winningCombs =
-  //
-  //   if winningCombos === 15{
-  //     alert("You Win!")
-  //   }else
 
-}
+
+
+
 // declare a winner or a scratch game
 
 // reset the board to start over

d870de09ae9a2b682ff510270d684dcf49d4e122
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-05-19 18:23:38 -0400

    Add scoring and refreshbutton

--- tttscripts.js
+++ tttscripts.js
@@ -1,86 +1,106 @@
 $(document).on('ready', function(){
 // set the turn counter to start at 0
+  // alert("Welcome to The Wilderness!\nWho will win? The camper or the bear?");
   var turns = 1;
-  var winningCombos = [[]];
+  var playerX = "x";
+  var playerO = "O";
+  // var playerX = prompt("Camper - please enter your name:");
+  //   $('#playerX').html(playerX);
+  // var playerO = prompt("Bear - please enter your name:");
+  //   $('#playerO').html(playerO);
 
-
-  var containerX = [];
-  var scoreX = 0;
   var containerO = [];
+  var scoreX = 0;
+  var containerX = [];
   var scoreO = 0;
+
+
 
   // Click to create X or O
   $('td').on('click', function(){
   // setting a variable. we want to do the work and do it once
+    turns++;
     var oldCell = $(this);
     // determin the turn - even turns the click returns an x and on odd turn the click returns an o
     if ( turns % 2 ){
-      oldCell.replaceWith("<td class='squareXO'> O </td>");
-      containerO.push(oldCell[0].innerText);
-      checkForWinner(containerO);
+      // oldCell.replaceWith("<td class='squareXO'> O </td>");
+      containerO.push(oldCell[0].className);
+      oldCell.addClass("squareO").html("O");
+      if ( (turns/2) >= 3 ){
+        convertScore(containerO);
+      }
 
     } else {
-      oldCell.replaceWith("<td class='squareXO'> X </td>");
-      containerX.push(oldCell[0].innerText);
-      checkForWinner(containerX);
+      // oldCell.replaceWith("<td class='squareXO'> X </td>");
+      containerX.push(oldCell[0].className);
+      oldCell.addClass("squareX").html("X");
+      if ( (turns/2) >= 3 ){
+        convertScore(containerX);
+      }
     }
     // this removes the event listener so we can only click once.
-    oldCell.off('click');
+      oldCell.off('click');
 
-    turns++;
+
   });
 
-    function checkForWinner(scoreArray){
 
-        for (i = 0; i < scoreArray.length; i++){
-          var square = scoreArray[i];
-          var squareToNum = parseInt(square);
-        }
-          if (turns % 2){
-            scoreO += squareToNum;
-            console.log("Current score for O is " + scoreO);
-            checkScore(scoreO, scoreArray);
-          }else{
-            scoreX += squareToNum;
-            console.log("Current score for X is " + scoreX);
-            checkScore(scoreX, scoreArray);
+    $('#reset').on('click', function(){
+    location.reload();
+      // containerO = [];
+      // containerX = [];
+      // turns = 1;
+
+      // $('td').bind("click");
+    });
+
+  // Converts score to parseInt
+  function convertScore(scoreArray){
+
+    var someNum = scoreArray.map(function(num){
+                    return parseInt(num);
+                  });
+
+    // Calls the function to check winner
+    if(turns % 2){
+      checkWinner(someNum, playerO);
+      console.log("check winner");
+    }else {
+      checkWinner(someNum, playerX);
+      console.log("check winner");
+    }
+
+  }
+
+  // Checks for the winner
+  function checkWinner(someArray, player){
+
+    for (var a = 0; a < someArray.length; a++) {
+      for (var b = a + 1; b < someArray.length; b++) {
+        for (var c = b + 1; c < someArray.length; c++) {
+          if ((someArray[a]) + (someArray[b]) + (someArray[c]) === 15) {
+            alert(` ${player} Wins!`);
+            if(turns % 2){
+              scoreO++;
+              updateScore(scoreO);
+            } else {
+              scoreX++;
+              updateScore(scoreX);
+            }
+            return;
           }
         }
+      }
+    }
+    if (turns === 10) {
+      alert("Nobody winns. Wanna play again?");
+    }
+}
 
-        function checkScore(someScore, someContainer){
+  function updateScore(playerScore){
+    $('#scoreO').html(scoreO);
+    $('#scoreX').html(scoreX);
+  }
 
-            if(someScore >= 15 && jQuery.inArray([2, 7, 6], someContainer)){
-              alert("you won 1!");
-            }else if(someScore >= 15 && jQuery.inArray([9, 5, 1], someContainer)){
-              alert("you won 2!");
-            }else if(someScore >= 15 && jQuery.inArray([4, 3, 8], someContainer)){
-              alert("you won 3!");
-            }else if(someScore >= 15 && jQuery.inArray([2, 9, 4], someContainer)){
-              alert("you won 4!");
-            }else if(someScore >= 15 && jQuery.inArray([7, 5, 3], someContainer)){
-              alert("you won 5!");
-            }else if(someScore >= 15 && jQuery.inArray([6, 1, 8], someContainer)){
-              alert("you won 6!");
-            }else if(someScore >= 15 && jQuery.inArray([2, 5, 8], someContainer)){
-              alert("you won 7!");
-            }else if(someScore >= 15 && jQuery.inArray([6, 5, 4], someContainer)){
-              alert("you won 2!");
-            }else if (someScore === 45){
-            alert("No Winner.");
-          }
-          }
 
 });
-
-// specifiy order of turns
-
-// create a loop to allow the users to keep playing untill there is a winner
-
-//evaluate to see if there is a winner
-
-
-
-
-// declare a winner or a scratch game
-
-// reset the board to start over

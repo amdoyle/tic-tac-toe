e5b2a2ddc0670501e28546c2b2d2916ccfefdfa7
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-05-23 21:30:59 -0400

    Cleaning up file and adding comments

--- script/tttscripts.js
+++ script/tttscripts.js
@@ -1,13 +1,16 @@
+// This game uses a magic square to tally winners for the game
 $(document).on('ready', function(){
-// set the turn counter to start at 0
+  // Alert to welcome players - currently disable but may include later on
   // alert("Welcome to The Wilderness!\nWho will win? The camper or the bear?");
+
   var turns = 1;
   var playerX = "x";
   var playerO = "O";
-  // var playerX = prompt("Camper - please enter your name:");
-  //   $('#playerX').html(playerX);
-  // var playerO = prompt("Bear - please enter your name:");
-  //   $('#playerO').html(playerO);
+  var playerX = prompt("Camper - please enter your name:");
+  var playerO = prompt("Bear - please enter your name:");
+  // This code will be implimented once the player scoreboard is implimented
+    // $('#playerX').html(playerX);
+    // $('#playerO').html(playerO);
 
   var containerO = [];
   var scoreX = 0;
@@ -18,38 +21,41 @@ $(document).on('ready', function(){
 
   // Click to create X or O
   $('td').on('click', function(){
-  // setting a variable. we want to do the work and do it once
     turns++;
     var oldCell = $(this);
     // determin the turn - even turns the click returns an x and on odd turn the click returns an o
     if ( turns % 2 ){
-      // oldCell.replaceWith("<td class='squareXO'> O </td>");
       containerO.push(oldCell[0].className);
       oldCell.addClass("squareO").html('<img class="bear" src="images/bear.svg" alt="bear played" />');
+      // Scores will only be convered if the turns are more than 3 - the
+      // convertScore method calls the checkWinner function
       if ( (turns/2) >= 3 ){
         convertScore(containerO);
       }
 
     } else {
-      // oldCell.replaceWith("<td class='squareXO'> X </td>");
       containerX.push(oldCell[0].className);
       oldCell.addClass("squareX").html('<img class="foot" src="images/foot.svg" alt="human played" />');
+
       if ( (turns/2) >= 3 ){
         convertScore(containerX);
       }
+
     }
-    // this removes the event listener so we can only click once.
+    // This removes the event listener so we can only click once
       oldCell.off('click');
 
 
   });
 
-
-    $('#reset').on('click', function(){
+  // Reset listener to reload the game board.
+  // Currently this works by reloading the page.
+  // This may need to be adjusted to impliment player score board
+  $('#reset').on('click', function(){
     location.reload();
-    });
+  });
 
-  // Converts score to parseInt
+  // Converts score to a number
   function convertScore(scoreArray){
 
     var someNum = scoreArray.map(function(num){
@@ -72,7 +78,7 @@ $(document).on('ready', function(){
       for (var b = a + 1; b < someArray.length; b++) {
         for (var c = b + 1; c < someArray.length; c++) {
           if ((someArray[a]) + (someArray[b]) + (someArray[c]) === 15) {
-            alert(` ${player} Wins!`);
+            alert(`${player} Wins!`);
             if(turns % 2){
               scoreO++;
               updateScore(scoreO);
@@ -90,10 +96,11 @@ $(document).on('ready', function(){
     }
 }
 
-  function updateScore(playerScore){
-    $('#scoreO').html(scoreO);
-    $('#scoreX').html(scoreX);
-  }
+  // This function may be implimented later once the player score board is functioning
+  // function updateScore(playerScore){
+  //   $('#scoreO').html(scoreO);
+  //   $('#scoreX').html(scoreX);
+  // }
 
 
 });
